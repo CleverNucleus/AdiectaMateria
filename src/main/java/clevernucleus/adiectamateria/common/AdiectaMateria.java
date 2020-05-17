a85681318ab00890a0c5da7f94b4b0264d1e52a1
CleverNucleus <jamie@mclean007.plus.com>
2020-05-17 17:15:18 +0100

    Initial Modspace

---
+++ src/main/java/clevernucleus/adiectamateria/common/AdiectaMateria.java
@@ -0,0 +1,10 @@
+package clevernucleus.adiectamateria.common;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AdiectaMateria.MODID)
+public class AdiectaMateria {
+	public static final String MODID = "adiectamateria";
+	
+	public AdiectaMateria() {}
+}
---
+++ src/main/java/clevernucleus/adiectamateria/common/init/Registry.java
@@ -0,0 +1,5 @@
+package clevernucleus.adiectamateria.common.init;
+
+public class Registry {
+	
+}
