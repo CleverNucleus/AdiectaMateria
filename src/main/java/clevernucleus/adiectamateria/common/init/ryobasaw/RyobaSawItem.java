d34108737a2384b5ad0c3accf092e15a5ab39ba3
CleverNucleus <jamie@mclean007.plus.com>
2020-08-04 13:50:25 +0100

    Removed PairResult Object
    -Removed PairResult object: It was only used in one circumstance and was messy code; replaced the use of this object with the classier (lol) Dual<A, B> object.
    *Changed the use of PairResult in RyobaSawItem to Dual

--- src/main/java/clevernucleus/adiectamateria/common/init/ryobasaw/RyobaSawItem.java
+++ src/main/java/clevernucleus/adiectamateria/common/init/ryobasaw/RyobaSawItem.java
@@ -3,7 +3,7 @@ package clevernucleus.adiectamateria.common.init.ryobasaw;
 import java.util.Map;
 
 import clevernucleus.adiectamateria.common.init.Registry;
-import clevernucleus.adiectamateria.common.util.PairResult;
+import clevernucleus.adiectamateria.common.util.Dual;
 import clevernucleus.adiectamateria.common.util.Util;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -22,17 +22,27 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class RyobaSawItem extends AxeItem {
-	private static final Map<Block, PairResult<IItemProvider>> RECIPES = Util.map(var -> {
-		var.put(Registry.WOVEN_TILE, PairResult.make(PairResult.P(Registry.FINE_SHOJI_PANEL, 12), PairResult.P(Registry.SHOJI_PANEL, 12)));
-		var.put(Blocks.ICE, PairResult.make(PairResult.P(Items.SNOWBALL, 4)));
+	private static final Map<Block, Dual<Dual<IItemProvider, Integer>, Dual<IItemProvider, Integer>>> RECIPES = Util.map(var -> {
+		var.put(Registry.WOVEN_TILE, Dual.get(() -> {
+			Dual<IItemProvider, Integer> var0 = Dual.make(Registry.SHOJI_PANEL, Integer.valueOf(12));
+			Dual<IItemProvider, Integer> var1 = Dual.make(Registry.FINE_SHOJI_PANEL, Integer.valueOf(12));
+			
+			return Dual.make(var0, var1);
+		}));
+		var.put(Blocks.ICE, Dual.get(() -> {
+			Dual<IItemProvider, Integer> var0 = Dual.make(Items.SNOWBALL, Integer.valueOf(4));
+			Dual<IItemProvider, Integer> var1 = Dual.make(Items.SNOWBALL, Integer.valueOf(4));
+			
+			return Dual.make(var0, var1);
+		}));
 	});
 	
 	public RyobaSawItem(Properties par0) {
 		super(ItemTier.GOLD, 3F, -2.4F, par0);
 	}
 	
-	private void cutBlock(World par0, BlockPos par1, final PairResult.Pair<IItemProvider> par2) {
-		ItemEntity var0 = new ItemEntity(par0, par1.getX(), par1.getY(), par1.getZ(), new ItemStack(par2.key(), par2.value()));
+	private void cutBlock(World par0, final BlockPos par1, final Dual<IItemProvider, Integer> par2) {
+		ItemEntity var0 = new ItemEntity(par0, par1.getX(), par1.getY(), par1.getZ(), new ItemStack(par2.a(), par2.b().intValue()));
 		
 		par0.setBlockState(par1, Blocks.AIR.getDefaultState(), 11);
 		par0.addEntity(var0);
@@ -50,7 +60,9 @@ public class RyobaSawItem extends AxeItem {
 			PlayerEntity var3 = par0.getPlayer();
 			
 			if(var3 != null) {
-				this.cutBlock(var0, var1, RECIPES.get(var2).get(var3.isCrouching()));
+				Dual<Dual<IItemProvider, Integer>, Dual<IItemProvider, Integer>> var4 = RECIPES.get(var2);
+				
+				this.cutBlock(var0, var1, var3.isCrouching() ? var4.a() : var4.b());
 				
 				par0.getItem().damageItem(1, var3, var -> {
 					var.sendBreakAnimation(par0.getHand());
