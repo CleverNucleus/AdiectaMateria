bfe42afd92407894a134d6e78a2ca775cf050308
CleverNucleus <jamie@mclean007.plus.com>
2021-06-29 16:35:49 +0100

    Reset for fabric 1.16.5

--- src/main/java/clevernucleus/adiectamateria/common/init/shoji/ShojiBlock.java
+++
@@ -1,9 +0,0 @@
-package clevernucleus.adiectamateria.common.init.shoji;
-
-import net.minecraft.block.PaneBlock;
-
-public class ShojiBlock extends PaneBlock {
-	public ShojiBlock(Properties par0) {
-		super(par0);
-	}
-}
---
+++ src/main/java/com/github/clevernucleus/adiectamateria/AdiectaMateria.java
@@ -0,0 +1,12 @@
+package com.github.clevernucleus.relicex;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class AdiectaMateria implements ModInitializer {
+	public static final String MODID = "relicex";
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+}
